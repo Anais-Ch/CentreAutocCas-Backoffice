d1fde1186db5861b65c812f0b6f4d3a8b7743c51
Anais-Ch <anais.chazeix@orange.fr>
2021-02-12 13:07:28 +0100

    add delete function on user page + redirectionc once users is deleted

--- src/app/Users/user/user.component.ts
+++ src/app/Users/user/user.component.ts
@@ -3,6 +3,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { UserJsonld } from 'src/app/models/user-jsonld';
 import { User } from 'src/app/models/users';
 import {ActivatedRoute, Router} from '@angular/router';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { ConstraintViolationList } from 'src/app/models/constraint-violation-list';
 
 
 @Component({
@@ -12,7 +14,8 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class UserComponent implements OnInit {
 
-    
+  public violationList: ConstraintViolationList|null = null; //declare violationList var for Input correspondance
+  closeResult: string|undefined; //for modal
     
  // @Input()
   //public user: User|null= null; //get User data in user var
@@ -35,7 +38,8 @@ export class UserComponent implements OnInit {
   constructor(
     private httpClient: HttpClient, //add httpclient protocole to component
     public activatedRoute: ActivatedRoute,     
-    public router: Router,    
+    public router: Router,
+    private modalService: NgbModal, //for pop up   
   ) { }
     
 
@@ -53,9 +57,66 @@ export class UserComponent implements OnInit {
 
     });
 
-
-    
+       
   }
+
+  //Method DeleteUSer
+    //retrieve user id in html call of the function
+    public deleteUser(id: number): void {
+      this.httpClient.delete('https://hb-bc-dwwm-2020.deploy.this-serv.com/api/users/'+ id).subscribe({
+        next : () => {
+
+          //redirection
+          this.router.navigate(['/users/users-list']);
+        },
+        error : (err: HttpErrorResponse) => { //error message
+          if (err.status === 404) {
+            this.violationList = err.error; //retrieve error form api message
+            alert (err.error['hydra:description']); 
+           
+          }
+          else { // inform iuser that an error has occured (need to dispaly a better message (error unexpected))
+            alert(err.status + '- An error as occured.');
+          }
+        },
+      });
+    }
+
+   /////// FUNCTION FOR MODAL
+   open(content: any) {
+
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+
+      this.closeResult = `Closed with: ${result}`;
+
+    }, (reason) => {
+
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+
+    });
+
+  }
+
+  
+  ////MODALS
+  private getDismissReason(reason: any): string {
+
+    if (reason === ModalDismissReasons.ESC) {
+
+      return 'by pressing ESC';
+
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+
+      return 'by clicking on a backdrop';
+
+    } else {
+
+      return  `with: ${reason}`;
+
+    }
+
+  }
+ 
 
   
 
